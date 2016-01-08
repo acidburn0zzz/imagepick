5a204395b5d790e73d501865f41ea5d31862c034
neymarsabin <reddevil.sabin@gmail.com>
2016-01-08 15:56:31 +0545

    add about page link to

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-   get 'home/index'
+
+   get 'home/about'
    root 'home#index'
-  # The priority is based upon order of creation: first created -> highest priority.
+
+
+   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
